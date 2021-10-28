2e5a9fe3717f4f21245e15424d315b94b899b4d3
adit450 <vaswani.a@husky.neu.edu>
2021-10-28 05:01:06 -0400

    Code cleaned

--- js/brushinglinkingstarter.js
+++ js/brushinglinkingstarter.js
@@ -2,7 +2,7 @@ var margin = { top: 10, right: 30, bottom: 50, left: 60 },
   width = 460 - margin.left - margin.right,
   height = 450 - margin.top - margin.bottom;
 
-// append svg object to the body of the page to house Scatterplot 1
+// Append svg object to the body of the page to house Scatterplot 1
 var svg1 = d3
   .select("#dataviz_brushScatter")
   .append("svg")
@@ -45,7 +45,7 @@ d3.csv("data/iris.csv").then((data) => {
     var xKey1 = "Sepal_Length";
     var yKey1 = "Petal_Length";
 
-    //Add X axis
+    //1. Add X axis
     var x1 = d3
       .scaleLinear()
       .domain(d3.extent(data.map((val) => val[xKey1])))
@@ -64,7 +64,7 @@ d3.csv("data/iris.csv").then((data) => {
           .text(xKey1)
       );
 
-    //Add Y axis
+    //2. Add Y axis
     var y1 = d3
       .scaleLinear()
       .domain(d3.extent(data.map((val) => val[yKey1])))
@@ -82,7 +82,7 @@ d3.csv("data/iris.csv").then((data) => {
           .text(yKey1)
       );
 
-    // Add dots
+    //3. Add dots
     var myCircle1 = svg1
       .append("g")
       .selectAll("circle")
@@ -104,6 +104,7 @@ d3.csv("data/iris.csv").then((data) => {
 
     //TODO: Define a brush
 
+      //Brush has been defined as per resource provided
       var brush1 =
           d3.brush()                 // Add the brush feature using the d3.brush function
               .extent( [ [0,0], [width,height] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
@@ -111,7 +112,8 @@ d3.csv("data/iris.csv").then((data) => {
           )
 
     //TODO: Add brush to the svg
-    svg1.call(brush1)
+      //Brush has been called to svg as per resource provided
+      svg1.call(brush1)
     
   }
 
@@ -122,7 +124,7 @@ d3.csv("data/iris.csv").then((data) => {
         var xKey2 = "Sepal_Width";
         var yKey2 = "Petal_Width";
 
-        //Add X axis
+        //1. Add X axis
         var x2 = d3
             .scaleLinear()
             .domain(d3.extent(data.map((val) => val[xKey2])))
@@ -141,7 +143,7 @@ d3.csv("data/iris.csv").then((data) => {
                     .text(xKey2)
             );
 
-        //Add Y axis
+        //2. Add Y axis
         var y2 = d3
             .scaleLinear()
             .domain(d3.extent(data.map((val) => val[yKey2])))
@@ -159,7 +161,7 @@ d3.csv("data/iris.csv").then((data) => {
                     .text(yKey2)
             );
 
-        // Add dots
+        //3. Add dots
         var myCircle2 = svg2
             .append("g")
             .selectAll("circle")
@@ -195,8 +197,10 @@ d3.csv("data/iris.csv").then((data) => {
 
     //TODO: Barchart with counts of different species
 
+    //Barchart (Code is similar to IC4)
     {
-        //Set x,y Scales for the bar chart
+        //https://www.educative.io/blog/d3-js-tutorial-bar-chart
+        //1. Set x,y Scales for the bar chart
     let x = d3.scaleBand().range([0, width]).padding(0.13),
         y = d3.scaleLinear().range([height, 0]);
 
@@ -208,14 +212,18 @@ d3.csv("data/iris.csv").then((data) => {
         //Mapping Y Values from 0 to the max Y value + 10
         y.domain([0, 50]);
 
-        //https://www.educative.io/blog/d3-js-tutorial-bar-chart
-
 
         //Convert Species Data into {Specie, SpecieCount} Format. Similar to X,Y data format from IC4
+        //This step allows us to manipulate the data in a way that is convenient for our plotting purposes
+
+        //Create a list of the specie types
         var speciesList = data.map(function(d) { return d.Species; })
+        //Initialize each specie count as 0
         var setosa = 0
         var versicolor= 0
         var virginica = 0;
+
+        //Parse the list and count each occurence of each specie type
         speciesList.forEach(function (item) {
             if(item === "setosa") {
                 setosa++;
@@ -230,8 +238,10 @@ d3.csv("data/iris.csv").then((data) => {
             }
         });
 
+        //Create a new List which displays the specie count of each specie
         var speciesCount = [setosa, versicolor, virginica]
 
+        //The data which will be plotted can be then defined below as:
         var speciesData = [
             {"Specie" : "setosa",
             "SpecieCount" : speciesCount[0]},
@@ -243,7 +253,7 @@ d3.csv("data/iris.csv").then((data) => {
 
                             console.log(speciesData)
 
-        //Create the Bar Chart with speciesData
+        //Create the Bar Chart with speciesData defined
         var myBarChart1 = svg3.selectAll(".bar")
             .data(speciesData)
             .enter().append("rect")
@@ -309,7 +319,7 @@ d3.csv("data/iris.csv").then((data) => {
       //TODO: Select bars in bar chart based on species selected in Scatterplot 2
 
 
-        //
+        //This function is called to keep track of the species in our selectedSpecies Set
         function addSpecie(d) {
             if(isBrushed(extent, x2(d.Sepal_Width), y2(d.Petal_Width))) {
             selectedSpecies.add(d.Species)
